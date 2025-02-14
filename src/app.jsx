11956e17a0fe3a98e25ef62c834a02481a26c74c
Nate Spangler <spangler.nathan.7@gmail.com>
2025-02-13 21:12:46 -0700

    changes

--- src/app.jsx
+++ src/app.jsx
@@ -5,36 +5,39 @@ import './app.css';
 export default function App() {
   return (
     <div className="body bg-dark text-light">
+        <br></br>
         <header>
-                <br> </br>
-                <button className="shiny-cta"> Pong Chaos</button>
+            <button class="shiny-cta"> Pong Chaos</button>
             <nav>
-                    <menu>
-                        <a href="index.html">Home</a>
-                        <br></br>
-                        <a href="scores.html">Top Scores</a>
-                    </menu>
-            </nav>
+                <menu>
+                    <a href="index.html">Home</a>
+                    <br></br>
+                    <a href="scores.html">High Scores</a>
+                </menu>
+           </nav>
         </header>
-
-        <main className="body">App components go here</main>
-
-        <footer>\
-            <br> </br>
-            <br> </br>
-            <br> </br>
-            <br> </br>
-            <br> </br>
-            <br> </br>
-            <br> </br>
-            <br> </br>
+        <main>
+            <br></br>
+            <br></br>
+            <br></br>
+            Here is where main will go
+        </main>
+        <footer>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
             <p>Random Fact: </p>
-            <br> </br>
-            <br> </br>
-            <span>Nate Spangler</span>
-            <br />
-            <a  href="https://github.com/NateSpangler/startup">GitHub</a>
-        </footer>
+            <br></br>
+            <br></br>
+            <span class="text-reset">Nate Spangler</span>
+            <br></br>
+            <a href="https://github.com/NateSpangler/startup">Github</a>
+      </footer>
     </div>
   );
 }
